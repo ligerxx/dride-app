883afd189c9d86d0750905ba91794099983c23e0
Yossi <saoron@gmail.com>
2016-10-20 23:17:46 +0300

    Added resources

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
 
+
+  public data: any;
 
   public users = [
     { name: 'Jilles', age: 21 },
@@ -32,8 +38,34 @@ export class Page1 {
   }
 
 
-  constructor(public navCtrl: NavController) {
-    
+  load() { 
+    if (this.data) {
+      // already loaded data
+      return Promise.resolve(this.data);
+    }
+
+    // don't have the data yet
+    return new Promise(resolve => {
+      // We're using Angular HTTP provider to request the data,
+      // then on the response, it'll map the JSON data to a parsed JS object.
+      // Next, we process the data and resolve the promise with the new data.
+      this.http.get('https://agentloan.com.ge/agentLoan/4/giveOffer.php')
+        .map(res => res.json())
+        .subscribe(data => {
+          // we've got back the raw data, now generate the core schedule data
+          // and save the data for later reference
+          this.data = data;
+          resolve(this.data);
+        });
+    });
   }
+
+
+  constructor(public navCtrl: NavController, public http: Http) {
+      this.load()
+      .then(data => {
+        console.log(data)
+      });
+      }
 
 }
