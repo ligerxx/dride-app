d1b04bf091feba8e48b6453ce6284719f3b8a055
Yossi <saoron@gmail.com>
2016-10-30 21:29:06 +0200

    Receive new data format

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -4,6 +4,8 @@ import { NavController } from 'ionic-angular';
 
 import {VideoService} from '../../providers/video-service';
 
+import {Pipe, PipeTransform} from '@angular/core';
+
 
 @Component({
   selector: 'page-page1',
@@ -17,7 +19,7 @@ export class Page1 {
 
   public data: any;
   //public host: string = "http://192.168.2.2"; 
-  public host: string = "http://192.168.42.1"; 
+  public host: string = "http://192.168.42.1:9000"; 
   public videos: any;
 
   public vid: HTMLVideoElement;
@@ -25,20 +27,6 @@ export class Page1 {
   public progressBar: boolean[] = [];
   public progress: any = 50;
 
-
-
-  public users = [
-    { name: 'Jilles', age: 21 },
-    { name: 'Todd', age: 24 },
-    { name: 'as', age: 18 },
-    { name: 'Lidasdassa', age: 18 },
-    { name: 'Lidasdassa', age: 18 },
-    { name: 'Lidasdsadsa', age: 18 },
-    { name: 'Liasdassa', age: 18 },
-    { name: 'Lidasdassa', age: 18 },
-    { name: 'Li23423sa', age: 18 },
-    { name: 'Li34234sa', age: 18 }
-  ];
   
   doRefresh(refresher) {
     console.log('Begin async operation', refresher);
@@ -70,7 +58,6 @@ export class Page1 {
       this.videoService.load(this.host)
       .then(data => {
         this.videos = data
-        console.log(this.videos)
       }); 
 
 
