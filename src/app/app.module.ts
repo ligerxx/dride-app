742550268e6a9ccb960774039ffc4dc747640b97
Yossi <saoron@gmail.com>
2016-11-29 11:52:36 +0200

    Added sharing to the dride-network :car: :bus:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 
+
 @NgModule({
   declarations: [
     MyApp,
@@ -19,6 +20,6 @@ import { Page2 } from '../pages/page2/page2';
     Page1,
     Page2
   ],
-  providers: []
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
